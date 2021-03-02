eaab4a9dd7f20319e4ce1eda4a07aaa02667b259
adityap0808 <63993023+adityap0808@users.noreply.github.com>
2021-03-02 09:48:45 +0530

    Add files via upload

---
+++ Frame3.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.awt.*;
+
+public class frame3 {
+public static void main(String args[]){
+ frame3 f1=new frame3();
+f1.setTitle("Frame with title");
+ f1.setSize(400,250);
+ f1.setVisible(true);
+ }
+}
