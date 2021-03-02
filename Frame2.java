88722258ffd7b1a5100e420ff7a3f3451f5e7832
adityap0808 <63993023+adityap0808@users.noreply.github.com>
2021-03-02 09:48:05 +0530

    Add files via upload

---
+++ Frame2.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.awt.*;
+
+public class Frame2 extends Frame{
+public static void main(String args[]){
+ Frame2 f1=new Frame2();
+ f1.setSize(400,250);
+ f1.setVisible(true);
+ }
+}
